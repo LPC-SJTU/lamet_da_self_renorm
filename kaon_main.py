9cb2f2edf454289a06ca8301b624d9a2f8ce2873
Greyyy-HJC <hejinchen17@gmail.com>
2021-10-27 22:13:42 +0800

    kaon

--- kaon_main.py
+++ kaon_main.py
@@ -5,9 +5,9 @@ from meson_da_hyb_class import MESON_DA_HYB
 from plot import *
 np.set_printoptions(threshold=np.inf)
 
-meson = 'pion'
+meson = 'kaon'
 
-def pion_main():
+def kaon_main():
     zR_dic, m_pdf_dic = pdf_zR()
     print(zR_dic)
 
@@ -64,30 +64,30 @@ def pion_main():
     # print('mom=6:')
     # print( 'For lambda bigger than ' + str(lambda_ls[extend_point['mom=6']]) + ', use extrapolation function.' )
     # print( 'Lambda starts from ' + str(lambda_ls[extend_fit_start['mom=6']]) + ' are included in the extrapolation fit.' )
-    # pion_mom_6 = MESON_DA_HYB(meson, 6, x_ls, x_ls_matching, y_ls_matching, extend_point['mom=6'], extend_fit_start['mom=6'], t_dic['mom=6'], fit_1=False, fit_2=False, rotate=True, lcda_fit=True)
-    # mom_6_kernel, mom_6_quasi_da, mom_6_y_ls, mom_6_lic_da = pion_mom_6.main(zR_dic)
-    # gv.dump(mom_6_lic_da, 'pion/mom=6/mom_6_lic_da')
-    # quasi_vs_lc_plot(x_ls, mom_6_y_ls, mom_6_quasi_da, mom_6_lic_da, pz=1.29, meson='pion')
+    # kaon_mom_6 = MESON_DA_HYB(meson, 6, x_ls, x_ls_matching, y_ls_matching, extend_point['mom=6'], extend_fit_start['mom=6'], t_dic['mom=6'], fit_1=False, fit_2=False, rotate=False, lcda_fit=True)
+    # mom_6_kernel, mom_6_quasi_da, mom_6_y_ls, mom_6_lic_da = kaon_mom_6.main(zR_dic)
+    # gv.dump(mom_6_lic_da, 'kaon/mom=6/mom_6_lic_da')
+    # quasi_vs_lc_plot(x_ls, mom_6_y_ls, mom_6_quasi_da, mom_6_lic_da, pz=1.29, meson='kaon')
 
     # ################################################
     # lambda_ls = z_ls_da * 2*np.pi / (0.0574*96) * 8
     # print('mom=8:')
     # print( 'For lambda bigger than ' + str(lambda_ls[extend_point['mom=8']]) + ', use extrapolation function.' )
     # print( 'Lambda starts from ' + str(lambda_ls[extend_fit_start['mom=8']]) + ' are included in the extrapolation fit.' )
-    # pion_mom_8 = MESON_DA_HYB(meson, 8, x_ls, x_ls_matching, y_ls_matching, extend_point['mom=8'], extend_fit_start['mom=8'], t_dic['mom=8'], fit_1=False, fit_2=False, rotate=True, lcda_fit=True)
-    # mom_8_kernel, mom_8_quasi_da, mom_8_y_ls, mom_8_lic_da = pion_mom_8.main(zR_dic)
-    # gv.dump(mom_8_lic_da, 'pion/mom=8/mom_8_lic_da')
-    # quasi_vs_lc_plot(x_ls, mom_8_y_ls, mom_8_quasi_da, mom_8_lic_da, pz=1.72, meson='pion')
+    # kaon_mom_8 = MESON_DA_HYB(meson, 8, x_ls, x_ls_matching, y_ls_matching, extend_point['mom=8'], extend_fit_start['mom=8'], t_dic['mom=8'], fit_1=False, fit_2=False, rotate=False, lcda_fit=True)
+    # mom_8_kernel, mom_8_quasi_da, mom_8_y_ls, mom_8_lic_da = kaon_mom_8.main(zR_dic)
+    # gv.dump(mom_8_lic_da, 'kaon/mom=8/mom_8_lic_da')
+    # quasi_vs_lc_plot(x_ls, mom_8_y_ls, mom_8_quasi_da, mom_8_lic_da, pz=1.72, meson='kaon')
 
     # ################################################
     # lambda_ls = z_ls_da * 2*np.pi / (0.0574*96) * 10
     # print('mom=10:')
     # print( 'For lambda bigger than ' + str(lambda_ls[extend_point['mom=10']]) + ', use extrapolation function.' )
     # print( 'Lambda starts from ' + str(lambda_ls[extend_fit_start['mom=10']]) + ' are included in the extrapolation fit.' )
-    # pion_mom_10 = MESON_DA_HYB(meson, 10, x_ls, x_ls_matching, y_ls_matching, extend_point['mom=10'], extend_fit_start['mom=10'], t_dic['mom=10'], fit_1=False, fit_2=False, rotate=True, lcda_fit=True)
-    # mom_10_kernel, mom_10_quasi_da, mom_10_y_ls, mom_10_lic_da = pion_mom_10.main(zR_dic)
-    # gv.dump(mom_10_lic_da, 'pion/mom=10/mom_10_lic_da')
-    # quasi_vs_lc_plot(x_ls, mom_10_y_ls, mom_10_quasi_da, mom_10_lic_da, pz=1.72, meson='pion')
+    # kaon_mom_10 = MESON_DA_HYB(meson, 10, x_ls, x_ls_matching, y_ls_matching, extend_point['mom=10'], extend_fit_start['mom=10'], t_dic['mom=10'], fit_1=False, fit_2=False, rotate=False, lcda_fit=True)
+    # mom_10_kernel, mom_10_quasi_da, mom_10_y_ls, mom_10_lic_da = kaon_mom_10.main(zR_dic)
+    # gv.dump(mom_10_lic_da, 'kaon/mom=10/mom_10_lic_da')
+    # quasi_vs_lc_plot(x_ls, mom_10_y_ls, mom_10_quasi_da, mom_10_lic_da, pz=1.72, meson='kaon')
 
 
     ################################################
@@ -95,12 +95,8 @@ def pion_main():
     paper_plot_discrete_effect(mom=8)
     paper_plot_discrete_effect(mom=10)
 
-    continuous_limit_pz_mix(meson='pion', mom_ls=[6,8,10])
+    continuous_limit_pz_mix(meson='kaon', mom_ls=[6,8,10])
     lcda_mix_pz_plot(meson, mom_6_y_ls)
 
 if __name__ == '__main__':
-    pion_main()
-
-
-
-# %%
+    kaon_main()
